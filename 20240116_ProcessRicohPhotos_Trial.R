634226ceba63cabc9edc300ae0b74fd63a87b484
Amanda L. McCutcheon <almccutcheon@nps.gov>
2024-01-16 10:27:20 -1000

    initial commit

---
+++ 20240116_ProcessRicohPhotos_Trial.R
@@ -0,0 +1,24 @@
+# Libraries
+library(here)
+library(tidyverse)
+library(magick)
+library(exiftoolr)
+install_exiftool()
+
+# Working directory
+setwd(here("testphotos"))
+
+# Import photos
+file.names<-list.files(pattern="*.JPG")
+
+img <- image_read(file.names)
+
+metadata <- exif_read(file.names,tags=c("Park","Direction","Site","Photographer","PoolName","GPSLatitude","GPSLongitude","CreateDate","GPSDestBearing"))
+img_ann <- image_annotate(img,metadata[1,2],size = 250,
+                gravity = "northwest",
+                font = "Helvetica",
+                color = "white",
+                strokecolor = "black",
+                weight = 9000)
+
+image_write(img_ann, path = here("Out.jpg"), format = "jpg")
