3cc15cd505639f126b8b883a1a69cfcfba8aeeef
Amanda L. McCutcheon <almccutcheon@nps.gov>
2024-05-16 09:53:23 -1000

    small edit

--- 20240116_ProcessRicohPhotos_Trial.R
+++ 20240116_ProcessRicohPhotos_Trial.R
@@ -13,6 +13,7 @@ file.names<-list.files(pattern="*.JPG")
 
 img <- image_read(file.names)
 
+t <- exif_call(args=c("-s","Park"),file.names[1])
 metadata <- exif_read(file.names,tags=c("Park","Direction","Site","Photographer","PoolName","GPSLatitude","GPSLongitude","CreateDate","GPSDestBearing"))
 img_ann <- image_annotate(img,metadata[1,2],size = 250,
                 gravity = "northwest",
